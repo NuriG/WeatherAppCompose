bc7c9bf8378463020254f1eb13338fb719d72caa
Нургали Шавалиев <shavaliev888@mail.ru>
2022-07-20 12:41:48 +0300

    3 commit

--- app/src/main/java/com/example/weatherappcompose/MainActivity.kt
+++ app/src/main/java/com/example/weatherappcompose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.weatherappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weatherappcompose.ui.theme.WeatherAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +34,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier.fillMaxHeight(0.5f)
+            .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Text(text = "Temp in $name")
+        }
+        Box(
+            modifier = Modifier.fillMaxHeight()
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        ){
+            Button(onClick = {
+
+            }, modifier = Modifier.padding(5.dp)
+                .fillMaxWidth()
+            ) {
+                Text(text = "Refresh")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
